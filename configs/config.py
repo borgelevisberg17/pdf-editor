0c873e3ac84b62653a368ab9055ee572814c1640
Borge Levisberg <126169064+borgelevisberg17@users.noreply.github.com>
2025-09-12 17:34:04 +0100

    Update config.py
    Up

--- configs/config.py
+++ configs/config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = '7871726855:AAEZs-C_3qp7mWWxyaHcGMNrBGqsMBl5URc'
-CHAT_ID = '5913164677'
+TELEGRAM_TOKEN = 'seu_token'
+CHAT_ID = 'seuiddotelegram'
 # Configurações salvas
-CONFIG_FILE = "config_borgepdf.json"
+CONFIG_FILE = "configpdf.json"
